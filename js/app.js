90f432c4b83c36d6b92c7ab5ac1fe717da7ea6a8
Martin Papa <martin.papa@outlook.com>
2019-11-11 14:47:46 -0800

    added all stores in separate object

---
+++ js/app.js
@@ -0,0 +1,42 @@
+'use strict';
+const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12apm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm'];
+const locations = [];
+    
+// Locations 
+var storeOne = {
+    location: 'Seattle',
+    minCustomer: 23,
+    maxCustomer: 65,
+    averageSale: 6.3
+};
+
+var storeTwo = {
+    location: 'Tokyo',
+    minCustomer: 3,
+    maxCustomer: 24,
+    averageSale: 1.2
+};
+
+var storeThree = {
+    location: 'Dubai',
+    minCustomer: 11,
+    maxCustomer: 38,
+    averageSale: 3.7
+};
+
+var storeFour = {
+    location: 'Paris',
+    minCustomer: 20,
+    maxCustomer: 38,
+    averageSale: 2.3
+};
+
+var storeFive = {
+    location: 'Lima',
+    minCustomer: 2,
+    maxCustomer: 16,
+    averageSale: 4.6
+};
+
+
+
