7b09526181da4642f2310c2d3622be3d4922f4b5
Martin Papa <martin.papa@outlook.com>
2019-11-12 16:28:26 -0800

    stopping point, data is working but still gotta render to a table

--- js/app.js
+++ js/app.js
@@ -1,8 +1,6 @@
 'use strict';
 
-const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
-// var perHourSale = [];
-
+const hours = ['6:00 am', '7:00 am', '8:00 am', '9:00 am', '10:00 am', '11:00 am', '12:00 pm', '1:00 pm', '2:00 pm', '3:00 pm', '4:00 pm', '5:00 pm', '6:00 pm', '7:00 pm'];
 
 function Store(location , minCustomer, maxCustomer, averageSale, perHourSale = []){
     this.location = location;
@@ -23,62 +21,6 @@ var storeThree = new Store('Dubai', 11, 38, 3.7);
 var storeFour = new Store('Paris', 20, 38, 2.8);
 var storeFive = new Store('Lima', 2, 16, 4.6); 
 
-
-// var storeOne = {
-//     location: 'Seattle',
-//     minCustomer: 23,
-//     maxCustomer: 65,
-//     averageSale: 6.3,
-//     getRandomNumberofCustomers: function (min, max) {
-//         return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is exclusive and the minimum is inclusive
-//     },
-//     perHourSale: []
-// };
-
-// var storeTwo = {
-//     location: 'Tokyo',
-//     minCustomer: 3,
-//     maxCustomer: 24,
-//     averageSale: 1.2,
-//     getRandomNumberofCustomers: function (min, max) {
-//         return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is exclusive and the minimum is inclusive
-//     },
-//     perHourSale: []
-// };
-
-// var storeThree = {
-//     location: 'Dubai',
-//     minCustomer: 11,
-//     maxCustomer: 38,
-//     averageSale: 3.7,
-//     getRandomNumberofCustomers: function (min, max) {
-//         return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is exclusive and the minimum is inclusive
-//     },
-//     perHourSale: []
-// };
-
-// var storeFour = {
-//     location: 'Paris',
-//     minCustomer: 20,
-//     maxCustomer: 38,
-//     averageSale: 2.3,
-//     getRandomNumberofCustomers: function (min, max) {
-//         return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is exclusive and the minimum is inclusive
-//     },
-//     perHourSale: []
-// };
-
-// var storeFive = {
-//     location: 'Lima',
-//     minCustomer: 2,
-//     maxCustomer: 16,
-//     averageSale: 4.6,
-//     getRandomNumberofCustomers: function (min, max) {
-//         return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is exclusive and the minimum is inclusive
-//     },
-//     perHourSale: []
-// };
-
 // Functions
 
 function getPerHourSale(store) {
@@ -98,31 +40,83 @@ function getTotalSale(store) {
     return `Total: ${totalSale} cookies`;
 }
 
-function displaySales(store) {
-    var parentElement = document.getElementById('salesReport');
+// function displaySales(store) {
+//     var parentElement = document.getElementById('salesReport');
 
-    var article = document.createElement('article');
-    parentElement.appendChild(article)
+//     var article = document.createElement('article');
+//     parentElement.appendChild(article);
 
-    var storeHeading = document.createElement('h2');
-    storeHeading.textContent = store.location;
-    article.appendChild(storeHeading);
+//     var table = document.createElement('table');
+//     table.textContent = store.location;
+//     article.appendChild(table);
 
-    var ul = document.createElement('ul');
-    article.appendChild(ul);
+//     var ul = document.createElement('ul');
+//     article.appendChild(ul);
 
 
-    for (var i = 0; i < store.perHourSale.length; i++) {
-        var li = document.createElement('li');
-        li.textContent = `${hours[i]}: ${store.perHourSale[i]} cookies`;
-        ul.appendChild(li);
-    }
+//     for (var i = 0; i < store.perHourSale.length; i++) {
+//         var li = document.createElement('li');
+//         li.textContent = `${hours[i]}: ${store.perHourSale[i]} cookies`;
+//         ul.appendChild(li);
+//     }
 
-        var li = document.createElement('li')
-        li.textContent = getTotalSale(store);
-        ul.appendChild(li);
+//         var li = document.createElement('li')
+//         li.textContent = getTotalSale(store);
+//         ul.appendChild(li);
+    
+// }
+
+
+// Table 
+
+
+// Create New Row Function
+function createNewRow(row) {
+    var row = document.createElement('tr');
+    table.appendChild(row);
     
 }
+
+
+
+var parentElement = document.getElementById('salesReport');
+
+var table = document.createElement('table');
+parentElement.appendChild(table);
+
+var tableRow = document.createElement('tr');
+table.appendChild(tableRow);
+
+for(var i = 0; i < hours.length; i++) {
+    var tableHeading = document.createElement('th');
+    tableRow.appendChild(tableHeading);
+    tableHeading.textContent = hours[i];
+}
+
+    tableRow.appendChild(tableHeading);
+    tableHeading.textContent = 'Daily Location Total';    
+
+    var tableRow = document.createElement('tr');
+    table.appendChild(tableRow);
+
+
+var tableData = document.createElement('td')
+tableRow.appendChild(tableData);
+tableData.textContent = 'HI'
+
+
+// Table data
+
+    var tableData = document.createElement('td')
+
+
+// addElement(table, 'salesReport')
+// function addElement(tag, container, text) {
+//     var element = document.createElement(tag);
+//     container.appendChild(element);
+//     element.textContent = text;
+//     return element;
+// }
 // Function Invocations
 
 getPerHourSale(storeOne);
@@ -131,8 +125,8 @@ getPerHourSale(storeThree);
 getPerHourSale(storeFour);
 getPerHourSale(storeFive);
 
-displaySales(storeOne);
-displaySales(storeTwo);
-displaySales(storeThree);
-displaySales(storeFour);
-displaySales(storeFive);
+// displaySales(storeOne);
+// displaySales(storeTwo);
+// displaySales(storeThree);
+// displaySales(storeFour);
+// displaySales(storeFive);
